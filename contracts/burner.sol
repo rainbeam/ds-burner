f6647bdaef67eef37bcc49c24c1d79771f575b77
Daniel Brockman <daniel@brockman.se>
2016-08-30 03:14:41 +0200

    initial import

---
+++ contracts/burner.sol
@@ -0,0 +1,19 @@
+import 'ds-token/token.sol';
+import 'supply_manager.sol';
+
+contract DSTokenBurner
+{
+    DSToken _token;
+    DSTokenSupplyManager _supply_manager;
+    function DSTokenBurner(DSToken token, DSTokenSupplyManager supply_manager) {
+        _token = token;
+        _supply_manager = supply_manager;
+    }
+    function burn(uint amount) {
+        _token.transferFrom(msg.sender, this, amount);
+        _supply_manager.destroy(amount);
+    }
+    function burn() {
+        _supply_manager.destroy(_token.balanceOf(this));
+    }
+}
